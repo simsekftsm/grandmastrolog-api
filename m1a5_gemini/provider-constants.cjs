6ca8b7cc62d849a85d1f50bfe23e9b802d09cf08
simsekftsm <info.msimsek@gmail.com>
2026-09-18 20:21:30 +0300

    test(m1a5): pin isolated Gemini provider constants

---
+++ m1a5_gemini/provider-constants.cjs
@@ -0,0 +1,6 @@
+'use strict';
+module.exports = Object.freeze({
+  model: 'gemini-2.5-flash',
+  endpoint: 'https://generativelanguage.googleapis.com/v1beta2/interactions',
+  max_output_tokens: 8192
+});
