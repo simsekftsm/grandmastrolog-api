9874c9bc1631dd87e4bc12ff8b1072a3b731d3c1
simsekftsm <info.msimsek@gmail.com>
2026-09-18 20:22:21 +0300

    test(m1a5): annotate Gemini API surface

--- m1a5_gemini/provider-constants.cjs
+++ m1a5_gemini/provider-constants.cjs
@@ -2,5 +2,6 @@
 module.exports = Object.freeze({
   model: 'gemini-2.5-flash',
   endpoint: 'https://generativelanguage.googleapis.com/v1beta2/interactions',
-  max_output_tokens: 8192
+  max_output_tokens: 8192,
+  api: 'interactions'
 });
