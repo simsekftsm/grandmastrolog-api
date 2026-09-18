4b0b515f68bd5ac4196fdd29d24dd1a5af5770ef
simsekftsm <info.msimsek@gmail.com>
2026-09-18 20:22:45 +0300

    test(m1a5): reuse accepted generation schema

--- m1a5_gemini/provider-constants.cjs
+++ m1a5_gemini/provider-constants.cjs
@@ -1,4 +1,5 @@
 'use strict';
+const schemaSource = require('../delivery_runtime/lib/groq-binding');
 module.exports = Object.freeze({
   model: 'gemini-2.5-flash',
   endpoint: 'https://generativelanguage.googleapis.com/v1beta2/interactions',
